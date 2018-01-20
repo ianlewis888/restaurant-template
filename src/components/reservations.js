ba849e25145dea88add21e1f5a79e03390546c4e
Ian Lewis <ian.lewis748@gmail.com>
2018-01-20 14:41:44 -0500

    bound state to form data in reservations form

--- src/components/reservations.js
+++ src/components/reservations.js
@@ -7,7 +7,7 @@ export default class Reservations extends Component {
 
   constructor (props) {
     super(props)
-    this.state = {
+    this.cleared = {
       name: '',
       nameError: '',
       email: '',
@@ -19,17 +19,26 @@ export default class Reservations extends Component {
       selectedDate: undefined,
       selectedDateError: ''
     };
+    this.errorText = {
+      name: 'please enter a name',
+      email: 'please enter valid email',
+      phone: 'please enter valid US phone number',
+      invalidNumberOfPeople: 'please enter a valid number',
+      tooManyPeople: 'kindly call for parties of 20 or more',
+      selectedDate: 'please select a valid date'
+    }
     this.handleNameChange = this.handleNameChange.bind(this);
     this.handleEmailChange = this.handleEmailChange.bind(this);
     this.handlePhoneChange = this.handlePhoneChange.bind(this);
     this.handleNumberOfPeopleChange = this.handleNumberOfPeopleChange.bind(this);
     this.handleDatepickerChange = this.handleDatepickerChange.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
+    this.state = this.cleared;
   }
 
   handleNameChange(e) {
     const name = e.target.value;
-    const err = (!name) ? 'please enter a name' : '';
+    const err = (!name) ? this.errorText.name : '';
     this.setState({
       ...this.state,
       name: name,
@@ -40,7 +49,7 @@ export default class Reservations extends Component {
   handleEmailChange(e) {
     const regexp = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/g;
     const email = e.target.value;
-    const err = (!regexp.test(email)) ? 'please enter valid email' : '';
+    const err = (!regexp.test(email)) ? this.errorText.email : '';
     this.setState({
       ...this.state,
       email: email,
@@ -51,7 +60,7 @@ export default class Reservations extends Component {
   handlePhoneChange(e) {
     const regexp = /^(\+?0?1\s?)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/g;
     const phone = e.target.value;
-    const err = (!regexp.test(phone)) ? 'please enter valid US phone number' : '';
+    const err = (!regexp.test(phone)) ? this.errorText.phone : '';
     this.setState({
       ...this.state,
       phone: phone,
@@ -63,10 +72,10 @@ export default class Reservations extends Component {
     var err = '';
     const regexp = /^\d+$/g;
     const numberOfPeople = e.target.value;
-    if (!regexp.test(numberOfPeople) || Number(numberOfPeople) < 1) { err = 'please enter a valid number'; }
-    if (Number(numberOfPeople) > 19) {
-       err = 'kindly call for parties of 20 or more';
-     }
+    if (!regexp.test(numberOfPeople) || Number(numberOfPeople) < 1) {
+      err = this.errorText.invalidNumberOfPeople;
+    }
+    if (Number(numberOfPeople) > 19) { err = this.errorText.tooManyPeople; }
     this.setState({
       ...this.state,
       numberOfPeople: numberOfPeople,
@@ -75,7 +84,7 @@ export default class Reservations extends Component {
   }
 
   handleDatepickerChange(date) {
-    const err = (!date) ? 'please select a valid date' : '';
+    const err = (!date) ? this.errorText.selectedDate : '';
     this.setState({
       ...this.state,
       selectedDate: date,
@@ -86,6 +95,37 @@ export default class Reservations extends Component {
   onSubmit(e) {
     e.preventDefault();
     console.log(this.state);
+    var formError = false;
+    const errors = [
+      this.state.nameError,
+      this.state.emailError,
+      this.state.phoneError,
+      this.state.numberOfPeopleError,
+      this.state.selectedDateError
+    ];
+    errors.forEach(err => {
+      if (err) { formError = true; }
+    });
+    var empties = {};
+    const formData = {
+      name: this.state.name,
+      email: this.state.email,
+      phone: this.state.phone,
+      numberOfPeople: this.state.numberOfPeople,
+      selectedDate: this.state.selectedDate
+    };
+    for (const d in formData) {
+      if (!formData[d]) {
+        formError = true;
+        const err = d + "Error";
+        empties[err] = (d === "numberOfPeople") ? this.errorText.invalidNumberOfPeople : this.errorText[d];
+      }
+    }
+    this.setState({
+      ...this.state,
+      ...empties
+    });
+    if (!formError) { this.setState(this.cleared); }
   }
 
   render() {
@@ -93,13 +133,33 @@ export default class Reservations extends Component {
       <div className="reservations">
         <h2>Make a Reservation</h2>
         <form name="reservation-form" onSubmit={this.onSubmit}>
-          <input type="text" placeholder="your name" onChange={this.handleNameChange}></input>
+          <input
+            type="text"
+            placeholder="your name"
+            value={this.state.name}
+            onChange={this.handleNameChange}>
+          </input>
           <label className="error-text">{this.state.nameError}</label>
-          <input type="text" placeholder="your email address" onChange={this.handleEmailChange}></input>
+          <input
+            type="text"
+            placeholder="your email address"
+            value={this.state.email}
+            onChange={this.handleEmailChange}>
+          </input>
           <label className="error-text">{this.state.emailError}</label>
-          <input type="text" placeholder="your phone number" onChange={this.handlePhoneChange}></input>
+          <input
+            type="text"
+            placeholder="your phone number"
+            value={this.state.phone}
+            onChange={this.handlePhoneChange}>
+          </input>
           <label className="error-text">{this.state.phoneError}</label>
-          <input type="text" placeholder="number of people" onChange={this.handleNumberOfPeopleChange}></input>
+          <input
+            type="text"
+            placeholder="number of people"
+            value={this.state.numberOfPeople}
+            onChange={this.handleNumberOfPeopleChange}>
+          </input>
           <label className="error-text">{this.state.numberOfPeopleError}</label>
           <DatePicker
               showTimeSelect
